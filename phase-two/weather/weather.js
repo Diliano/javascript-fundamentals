52edee0df9d113d5fb425c36596704e6558e32b8
Dilan Patel <patel_dilan@hotmail.com>
2023-03-25 18:20:13 +0000

    'load(city)' method tested and implemented

--- phase-two/weather/weather.js
+++ phase-two/weather/weather.js
@@ -1,5 +1,17 @@
+const WeatherClient = require('./weatherClient');
+
 class Weather {
 
+  constructor(weatherClient) {
+    this.weatherClient = weatherClient;
+    this.weatherData = null;
+  }
+
+  async load(city) {
+    const weatherData = await this.weatherClient.fetchWeatherData(city);
+    this.weatherData = weatherData;
+  };
+ 
 };
 
 module.exports = Weather;
--- phase-two/weather/weather.test.js
+++ phase-two/weather/weather.test.js
@@ -2,11 +2,23 @@ const Weather = require('./weather');
 const WeatherClient = require('./weatherClient');
 
 describe('Weather', () => {
-
+  
   it('constructs', () => {
     const weatherClient = new WeatherClient();
     const weather = new Weather(weatherClient);
     expect(weather).toBeInstanceOf(Weather);
+  });
+
+  it('loads a city from the WeatherClient', async () => {
+    const mockWeatherClient = {
+      fetchWeatherData: jest.fn(),
+    };
+    mockWeatherClient.fetchWeatherData.mockResolvedValueOnce({
+      name: 'London',
+    });
+    const weather = new Weather(mockWeatherClient);
+    await weather.load('London');
+    expect(mockWeatherClient.fetchWeatherData).toHaveBeenCalledWith('London');
   });
 
 });
